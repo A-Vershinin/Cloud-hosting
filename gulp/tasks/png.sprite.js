56d685bd1e324567fc018acaa28397c8d0e20807
Anatoliy Vershinin <cata07@yandex.ru>
2017-10-10 15:18:24 +0300

    add blog

--- gulp/tasks/png.sprite.js
+++ gulp/tasks/png.sprite.js
@@ -3,11 +3,11 @@
 module.exports = function() {
   $.gulp.task('sprite:png', function() {
     var spriteData = $.gulp.src('./app/img/sprites/png/*.png').pipe($.gp.spritesmith({
-        imgName: 'png-sprite.png',
-        cssName: '_png-sprite.scss',
+        imgName: 'icons-sprite.png',
+        cssName: '_icons-sprite.scss',
         // cssFormat: "scss",
         algorithm: 'left-right',
-        padding: 40
+        padding: 60
     }));
 
     spriteData.css.pipe($.gulp.dest('app/styles/basic/'));
