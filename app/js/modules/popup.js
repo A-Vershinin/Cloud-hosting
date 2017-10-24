8af49a4d6ad1102f41290a8b7d9dade09ecb906f
Anatoliy Vershinin <cata07@yandex.ru>
2017-10-24 14:53:54 +0300

    update popup

--- app/js/modules/popup.js
+++ app/js/modules/popup.js
@@ -7,17 +7,21 @@
       var currentModal = $this.attr("data-popup");
       $(currentModal).fadeIn(timeShow);
       $(".overlay").fadeIn(timeShow);
+      $(".wrapper").addClass("wrapper--blured");
+
     });
 
     $("." + popupClose).on("click", function (e) {
       e.preventDefault();
       $(this).parent().fadeOut(timeHidden);
       $(".overlay").fadeOut(timeHidden);
+      $(".wrapper").removeClass("wrapper--blured");
     });
 
     $(".wrapper").on("click", ".overlay", function() {
       $(".modal").fadeOut(timeHidden);
       $(".overlay").fadeOut(timeHidden);
+      $(".wrapper").removeClass("wrapper--blured");
     });
   }
   window.togglePopup = togglePopup;
--- app/js/modules/textShow.js
+++ app/js/modules/textShow.js
@@ -18,3 +18,7 @@
   }
   window.showPostText = showPostText;
 }());
+
+(function () {
+
+}());
